51c06c4e4670513863c10f99f069d7197955d339
Claudio Menegotto <menegotto.claudio@gmail.com>
2022-06-08 16:28:47 +0200

    bug fix

--- content.js
+++ content.js
@@ -29,12 +29,31 @@ checkSettings();
   //on body child changed
   function observate() {
     
-    if(!injectedmenu && document.getElementsByClassName("FTFzP")[0]!=null){
+    var myMenu = null;
+
+    if(document.getElementsByClassName("FTFzP").length > 0){
+      if(document.getElementsByClassName("FTFzP").length > 1){
+        for( var i = 0; i<document.getElementsByClassName("FTFzP").length; i++){
+          if(document.getElementsByClassName("FTFzP")[i].parentElement.getAttribute("class") == "simplebar-content"){
+            myMenu = document.getElementsByClassName("FTFzP")[i];
+          }
+        }
+      }else{
+        if(document.getElementsByClassName("FTFzP")[0].parentElement.getAttribute("class") == "simplebar-content"){
+            myMenu = document.getElementsByClassName("FTFzP")[0];
+        }else{
+          myMenu == null;
+        }
+      }
+
+    }
+
+    if(!injectedmenu && myMenu != null ){
       injectedmenu = true;
       checkSettings();
-      injectSettings();
+      injectSettings(myMenu);
     }else{
-      if(injectedmenu && document.getElementsByClassName("FTFzP")[0]==null){
+      if(injectedmenu && myMenu == null){
         injectedmenu = false;
       }
     }
@@ -55,7 +74,7 @@ checkSettings();
   }
 
   //function that inject new settings in profile menu
-  function injectSettings(){  
+  function injectSettings(myMenu){  
 
     var mySettingsContainer = document.createElement("div");
 
@@ -63,7 +82,7 @@ checkSettings();
 
     mySettingsContainer.innerHTML = "<div class='Layout-sc-nxg1ff-0 dwuicp'><div class='Layout-sc-nxg1ff-0 gcwIMz'><div class='Layout-sc-nxg1ff-0 hsTxrV'><div class='Layout-sc-nxg1ff-0 gcwIMz InjectLayout-sc-588ddc-0 tw-drop-down-menu-item-figure'><div class='ScIconLayout-sc-1bgeryd-0 cMFgBQ'><div class='ScAspectRatio-sc-1sw3lwy-1 kPofwJ tw-aspect'><div class='ScAspectSpacer-sc-1sw3lwy-0 dsswUS'></div><svg width='100%' height='100%' version='1.1' viewBox='0 0 20 20' x='0px' y='0px' class='ScIconSVG-sc-1bgeryd-1 ifdSJl'><g><path d='M10 6a4 4 0 014 4h-2a2 2 0 00-2-2V6z'></path><path fill-rule='evenodd' d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-2 0a6 6 0 11-12 0 6 6 0 0112 0z' clip-rule='evenodd'></path></g></svg></div></div></div></div><label class='ScDropDownMenuInputItemLabel-sc-p9ci3m-0 jmXxSR InjectLayout-sc-588ddc-0 kikCET tw-drop-down-menu-input-item__label' for='6a306ca0ecfbb87623b03e470dd5c09c'> Grabber Enable</label><div class='ScToggle-sc-796zbf-0 AgPgA tw-toggle' data-a-target='dark-mode-toggle' data-test-selector='user-menu__dark-mode-toggle'><input class='ScToggleInput-sc-796zbf-1 cwUJld tw-toggle__input' id='6a306ca0ecfbb87623b03e470dd5c09c' type='checkbox' data-a-target='tw-toggle'><label id='GrabberEnabled' for='6a306ca0ecfbb87623b03e470dd5c09c' class='ScToggleButton-sc-796zbf-2 dEPNHE tw-toggle__button'></label></div></div></div>";
 
-    document.getElementsByClassName("FTFzP")[0].childNodes[0].appendChild(mySettingsContainer);
+    myMenu.childNodes[0].appendChild(mySettingsContainer);
 
     if(grabbedEnabled)
     document.getElementById("GrabberEnabled").click(false);
